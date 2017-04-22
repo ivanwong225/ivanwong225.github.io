c49f734e0e999a11dd28db27122838e989b2eb98
Ivan Wong <wongi@bu.edu>
2017-04-22 15:23:07 -0400

    Update and rename indvproject.js to projectsCtrl.js

--- controller/projectsCtrl.js
+++ controller/projectsCtrl.js
@@ -1,12 +1,12 @@
 var app = angular.module('myApp');
 
-app.controller('ProjectController', ['$scope', '$http', 'streamFactory', function($scope, $http, photoFactory) {
+app.controller('ProjectController', ['$scope', '$http', 'streamFactory', function($scope, $http, streamFactory) {
 
   $scope.projectList = [];
   $scope.projects = [];
   $http({method: 'GET', url: 'json/projects.json'}).success(function(response) {
     $scope.projectList = response.projects;
-    photoFactory.addToView($scope.projectList, 
+    streamFactory.addToView($scope.projectList, 
                           $scope.projects, 
                           $scope.projectList.length, 
                           2, 0);
