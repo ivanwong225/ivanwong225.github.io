96817aa63004369ef672d55899ebef731a55a370
Ivan Wong <wongi@bu.edu>
2017-04-22 14:37:39 -0400

    Create photoFactory.js

---
+++ model/photoFactory.js
@@ -0,0 +1,21 @@
+var app = angular.module('myApp');
+
+app.factory('photoFactory', function() {
+  var service = {};
+
+  service.addToView = function(source, viewArray, numberToAdd, rowSize, offset) {
+    var row = [];
+    for(i = offset; i < offset + numberToAdd; i++) {
+      row.push(source[i]);
+      if(row.length === rowSize) {
+        viewArray.push(row);
+        row = [];
+      }
+    }
+    if(row.length !== 0) {
+      viewArray.push(row);
+    }
+  };
+
+  return service;
+});
