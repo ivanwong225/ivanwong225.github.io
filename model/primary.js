2281b345e745f4ada4038ec66a4aedb017c537b3
Ivan Wong <wongi@bu.edu>
2017-04-22 14:36:15 -0400

    Create primary.js

---
+++ model/primary.js
@@ -0,0 +1,25 @@
+var app = angular.module('myApp');
+
+app.factory('primary', ['$q', '$http', 'filterFilter', function ($q, $http, filterFilter) {
+  var service = {};
+
+  service.getEvents = function (path) {
+    return $http.get(path).then(function (response) {
+      return response.data.events;
+    });
+  };
+
+  service.getFood = function(path) {
+    return $http.get(path).then(function (response) {
+      return filterFilter(response.data.events, {category:'food'});
+    });
+  };
+
+  service.getPlaces = function(path) {
+    return $http.get(path).then(function (response) {
+      return filterFilter(response.data.events, {category:'place'});
+    });
+  };
+  
+  return service;
+}]);
