8a24640e16679281b0ff613fde5a6cdb0744fbac
Ivan Wong <wongi@bu.edu>
2017-04-22 14:40:45 -0400

    Create navigatorService.js

---
+++ service/navigatorService.js
@@ -0,0 +1,16 @@
+var navigationOptions = [{
+  category : "MYSELF",
+  path : "myself/school",
+  subsections : [
+    { label : "School", path : "myself/school"},
+    { label : "Projects", path : "myself/projects"}
+  ]
+},
+{
+  category : "WHERE",
+  path : "wibwit",
+  subsections : [
+    { label : "Food", path : "wibwit/food"},
+    { label : "Places", path : "wibwit/places"}
+  ]
+}];
